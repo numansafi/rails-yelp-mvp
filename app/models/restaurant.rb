63928e504c4fa6eeb648878fae03d0994d5d9d9d
Numan Safi <numansafi@live.com>
2021-08-18 16:54:34 +0100

    Completed yelp mvp build

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
+  validates :category, inclusion: {in: ["chinese", "italian", "japanese", "french", "belgian"]}
 end
